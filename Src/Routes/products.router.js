c17da9cd47e49aa0342d67bb878594f5d3897ff8
Nicolas <nicolascornejo007@gmail.com>
2024-11-19 23:39:52 -0300

    proyecto-backend

---
+++ Src/Routes/products.router.js
@@ -0,0 +1,88 @@
+import { Router } from 'express';
+import ProductManager from '../service/ProductManager.js';
+
+
+
+const router = Router();
+
+const productManager = new ProductManager()
+
+
+
+// -------------- listar --------------
+router.get('/', async (req, res) => {
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit) : undefined
+        const products = await productManager.getAllProducts(limit)
+        res.json(products)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// -------------- Obtener producto por id --------------
+router.get('/:pid', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+        const product = await productManager.getProductById(productId);
+
+        if (!product) {
+            return res.status(404).send('Producto no encontrado');
+        }
+
+        res.json(product)
+    } catch (error) {
+        console.log(error);
+
+    }
+})
+
+// -------------- Crear producto --------------
+router.post('/', async (req, res) => {
+    try {
+        const { title, description, code, price, stock, category, thumbnails } = req.body
+        if (!title || !description || !code || !price || !stock || !category) {
+            return res.status(400).json({ error: 'Todos los campos son obligatorios excepto thumbnails' });
+        }
+
+        const newProduct = await productManager.addProduct({ title, description, code, price, stock, category, thumbnails });
+
+        res.status(201).json(newProduct)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+// -------------- Actualizar producto por id --------------
+router.put('/:pid', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+
+        const updatedProduct = await productManager.updateProduct(productId, req.body);
+        if (updatedProduct) {
+            res.json(updatedProduct);
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// -------------- Eliminar producto por id --------------
+router.delete('/:pid', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+        const deletedProduct = await productManager.deleteProduct(productId);
+        if (deletedProduct) {
+            res.json(deletedProduct);
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export default router;
---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import productsRouter from './routes/products.router.js';
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('ping', (req, res) => {
+    res.send('pong');
+})
+app.use('/api/products', productsRouter);
+
+
+const SERVER_PORT = 9090;
+app.listen(SERVER_PORT, () => {
+    console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
+});
