1596111c185513d8df64c6e99f1da4d98b22f1fb
Nicolas <nicolascornejo007@gmail.com>
2024-12-01 14:44:35 -0300

    proyecto 1-

--- Src/app.js
+++ Src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import productsRouter from './routes/products.router.js';
-
+import productsRouter from '../src/routes/products.router.js'
 const app = express();
 
 
@@ -13,7 +12,7 @@ app.get('ping', (req, res) => {
 app.use('/api/products', productsRouter);
 
 
-const SERVER_PORT = 9090;
+const SERVER_PORT = 8080;
 app.listen(SERVER_PORT, () => {
     console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
 });
